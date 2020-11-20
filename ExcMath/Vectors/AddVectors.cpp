1f76d5252dd4156a8256950a6d9838844738d772
Kutrowski <Dominik.Kutrowski@dnvgl.com>
2020-11-20 16:12:52 +0100

    AddVectors update:  - refactoring of AddVectors::AplusAonBwithCores3(AddA, AddB),  - refactoring of AddVectors::AplusBonBwithCores3(AddA, AddB),

--- ExcMath/Vectors/AddVectors.cpp
+++ ExcMath/Vectors/AddVectors.cpp
@@ -2,73 +2,50 @@
 #include <thread>
 
 namespace {
-	// initial values:
-	unsigned long long CarryUnsignedLongLong = 1000000000000000000;
-	unsigned long long* CarryULL = &CarryUnsignedLongLong;
-
-	// core 3:
-	unsigned long long RangeStartValue0 = 0;
-	unsigned long long RangeStartValue1 = 0;
-	unsigned long long RangeStartValue2 = 0;
-	unsigned long long RangeStartValue3 = 0;
-	unsigned long long Core1RestStartValue = 0;
-	unsigned long long Core2RestStartValue = 0;
-	unsigned long long Core3RestStartValue = 0;
-
-	unsigned long long* Range0 = &RangeStartValue0;
-	unsigned long long* Range1 = &RangeStartValue1;
-	unsigned long long* Range2 = &RangeStartValue2;
-	unsigned long long* Range3 = &RangeStartValue3;
-	unsigned long long* Core1Rest = &Core1RestStartValue;
-	unsigned long long* Core2Rest = &Core2RestStartValue;
-	unsigned long long* Core3Rest = &Core3RestStartValue;
-}
-
-namespace {
 	inline void AplusAonBCore1(
 		std::vector<unsigned long long>* Core1AddA,
 		std::vector<unsigned long long>* Core1AddB,
-		unsigned long long* Core1Start,
-		unsigned long long* Core1Stop,
-		unsigned long long* Core1Rest)
+		unsigned long long Core1Start,
+		unsigned long long Core1Stop,
+		int Core1Rest)
 	{
-		for (unsigned long long i = *Core1Start; i <= *Core1Stop; i++) {
-			(*Core1AddB)[i] = (*Core1AddA)[i] + (*Core1AddA)[i] + *Core1Rest;
-			*Core1Rest = 0;
-			if ((*Core1AddB)[i] >= *CarryULL) {
-				(*Core1AddB)[i] = (*Core1AddB)[i] - *CarryULL;
-				*Core1Rest = 1;
+		for (unsigned long long i  = Core1Start; i <= Core1Stop; i++) {
+			(*Core1AddB)[i] = (*Core1AddA)[i] + (*Core1AddA)[i] + Core1Rest;
+			Core1Rest = 0;
+			if ((*Core1AddB)[i] >= 1000000000000000000) {
+				(*Core1AddB)[i] = (*Core1AddB)[i] - 1000000000000000000;
+				Core1Rest = 1;
 			}
 		}
 	}
 	inline void AplusAonBCore2(
 		std::vector<unsigned long long>* Core2AddA,
 		std::vector<unsigned long long>* Core2AddB,
-		unsigned long long* Core2Start,
-		unsigned long long* Core2Stop,
-		unsigned long long* Core2Rest)
+		unsigned long long Core2Start,
+		unsigned long long Core2Stop,
+		int Core2Rest)
 	{
-		for (unsigned long long i = *Core2Start + 1; i <= *Core2Stop; i++) {
-			(*Core2AddB)[i] = (*Core2AddA)[i] + (*Core2AddA)[i] + *Core2Rest;
-			*Core2Rest = 0;
-			if ((*Core2AddB)[i] >= *CarryULL) {
-				(*Core2AddB)[i] = (*Core2AddB)[i] - *CarryULL;
-				*Core2Rest = 1;
+		for (unsigned long long i  = Core2Start + 1; i <= Core2Stop; i++) {
+			(*Core2AddB)[i] = (*Core2AddA)[i] + (*Core2AddA)[i] + Core2Rest;
+			Core2Rest = 0;
+			if ((*Core2AddB)[i] >= 1000000000000000000) {
+				(*Core2AddB)[i] = (*Core2AddB)[i] - 1000000000000000000;
+				Core2Rest = 1;
 			}
 		}
 	}
 	inline void AplusAonBCore3(
 		std::vector<unsigned long long>* Core3AddA,
 		std::vector<unsigned long long>* Core3AddB,
-		unsigned long long* Core3Start,
-		unsigned long long* Core3Stop,
-		unsigned long long* Core3Rest)
+		unsigned long long Core3Start,
+		unsigned long long Core3Stop,
+		int* Core3Rest)
 	{
-		for (unsigned long long i = *Core3Start + 1; i <= *Core3Stop; i++) {
+		for (unsigned long long i  = Core3Start + 1; i <= Core3Stop; i++) {
 			(*Core3AddB)[i] = (*Core3AddA)[i] + (*Core3AddA)[i] + *Core3Rest;
 			*Core3Rest = 0;
-			if ((*Core3AddB)[i] >= *CarryULL) {
-				(*Core3AddB)[i] = (*Core3AddB)[i] - *CarryULL;
+			if ((*Core3AddB)[i] >= 1000000000000000000) {
+				(*Core3AddB)[i] = (*Core3AddB)[i] - 1000000000000000000;
 				*Core3Rest = 1;
 			}
 		}
@@ -77,47 +54,47 @@ namespace {
 	inline void AplusBonBCore1(
 		std::vector<unsigned long long>* Core1AddA,
 		std::vector<unsigned long long>* Core1AddB,
-		unsigned long long* Core1Start,
-		unsigned long long* Core1Stop,
-		unsigned long long* Core1Rest)
+		long long Core1Start,
+		long long Core1Stop,
+		int Core1Rest)
 	{
-		for (unsigned long long i = *Core1Start; i <= *Core1Stop; i++) {
-			(*Core1AddB)[i] = (*Core1AddB)[i] + (*Core1AddA)[i] + *Core1Rest;
-			*Core1Rest = 0;
-			if ((*Core1AddB)[i] >= *CarryULL) {
-				(*Core1AddB)[i] = (*Core1AddB)[i] - *CarryULL;
-				*Core1Rest = 1;
+		for (long i = Core1Start; i <= Core1Stop; i++) {
+			(*Core1AddB)[i] = (*Core1AddB)[i] + (*Core1AddA)[i] + Core1Rest;
+			Core1Rest = 0;
+			if ((*Core1AddB)[i] >= 1000000000000000000) {
+				(*Core1AddB)[i] = (*Core1AddB)[i] - 1000000000000000000;
+				Core1Rest = 1;
 			}
 		}
 	}
 	inline void AplusBonBCore2(
 		std::vector<unsigned long long>* Core2AddA,
 		std::vector<unsigned long long>* Core2AddB,
-		unsigned long long* Core2Start,
-		unsigned long long* Core2Stop,
-		unsigned long long* Core2Rest) {
+		long long Core2Start,
+		long long Core2Stop,
+		int Core2Rest) {
 
-		for (unsigned long long i = *Core2Start + 1; i <= *Core2Stop; i++) {
-			(*Core2AddB)[i] = (*Core2AddB)[i] + (*Core2AddA)[i] + *Core2Rest;
-			*Core2Rest = 0;
-			if ((*Core2AddB)[i] >= *CarryULL) {
-				(*Core2AddB)[i] = (*Core2AddB)[i] - *CarryULL;
-				*Core2Rest = 1;
+		for (long i = Core2Start + 1; i <= Core2Stop; i++) {
+			(*Core2AddB)[i] = (*Core2AddB)[i] + (*Core2AddA)[i] + Core2Rest;
+			Core2Rest = 0;
+			if ((*Core2AddB)[i] >= 1000000000000000000) {
+				(*Core2AddB)[i] = (*Core2AddB)[i] - 1000000000000000000;
+				Core2Rest = 1;
 			}
 		}
 	}
 	inline void AplusBonBCore3(
 		std::vector<unsigned long long>* Core3AddA,
 		std::vector<unsigned long long>* Core3AddB,
-		unsigned long long* Core3Start,
-		unsigned long long* Core3Stop,
-		unsigned long long* Core3Rest) {
+		long long Core3Start,
+		long long Core3Stop,
+		int *Core3Rest) {
 
-		for (unsigned long long i = *Core3Start + 1; i <= *Core3Stop; i++) {
+		for (long i = Core3Start + 1; i <= Core3Stop; i++) {
 			(*Core3AddB)[i] = (*Core3AddB)[i] + (*Core3AddA)[i] + *Core3Rest;
 			*Core3Rest = 0;
-			if ((*Core3AddB)[i] >= *CarryULL) {
-				(*Core3AddB)[i] = (*Core3AddB)[i] - *CarryULL;
+			if ((*Core3AddB)[i] >= 1000000000000000000) {
+				(*Core3AddB)[i] = (*Core3AddB)[i] - 1000000000000000000;
 				*Core3Rest = 1;
 			}
 		}
@@ -128,11 +105,11 @@ namespace AddVectors {
 	void AplusAonA(std::vector<unsigned long long>* Adds)
 	{
 		int Rest = 0;
-		for (unsigned long long i = 0; i < (*Adds).size(); i++) {
+		for (unsigned long long i  = 0; i < (*Adds).size(); i++) {
 			(*Adds)[i] = (*Adds)[i] + (*Adds)[i] + Rest;
 			Rest = 0;
-			if ((*Adds)[i] >= *CarryULL) {
-				(*Adds)[i] = (*Adds)[i] - *CarryULL;
+			if ((*Adds)[i] >= 1000000000000000000) {
+				(*Adds)[i] = (*Adds)[i] - 1000000000000000000;
 				Rest = 1;
 			}
 		}
@@ -142,11 +119,11 @@ namespace AddVectors {
 	{
 		int Rest = 0;
 		(*AddB).resize(0);
-		for (unsigned long long i = 0; i < (*AddA).size(); i++) {
+		for (unsigned long long i  = 0; i < (*AddA).size(); i++) {
 			(*AddB).push_back((*AddA)[i] + (*AddA)[i] + Rest);
 			Rest = 0;
-			if ((*AddB)[i] >= *CarryULL) {
-				(*AddB)[i] = (*AddB)[i] - *CarryULL;
+			if ((*AddB)[i] >= 1000000000000000000) {
+				(*AddB)[i] = (*AddB)[i] - 1000000000000000000;
 				Rest = 1;
 			}
 		}
@@ -156,49 +133,49 @@ namespace AddVectors {
 	{
 		int Rest = 0;
 		if ((*AddA).size() == (*AddB).size()) {
-			for (unsigned long long i = 0; i < (*AddA).size(); i++) {
+			for (unsigned long long i   = 0; i < (*AddA).size(); i++) {
 				(*AddB)[i] = (*AddA)[i] + (*AddB)[i] + Rest;
 				Rest = 0;
-				if ((*AddB)[i] >= *CarryULL) {
-					(*AddB)[i] = (*AddB)[i] - *CarryULL;
+				if ((*AddB)[i] >= 1000000000000000000) {
+					(*AddB)[i] = (*AddB)[i] - 1000000000000000000;
 					Rest = 1;
 				}
 			}
 			if (Rest == 1) (*AddB).push_back(1);
 		}
 		else if ((*AddA).size() < (*AddB).size()) {
-			for (unsigned long long i = 0; i < (*AddA).size(); i++) {
+			for (unsigned long long i  = 0; i < (*AddA).size(); i++) {
 				(*AddB)[i] = (*AddA)[i] + (*AddB)[i] + Rest;
 				Rest = 0;
-				if ((*AddB)[i] >= *CarryULL) {
-					(*AddB)[i] = (*AddB)[i] - *CarryULL;
+				if ((*AddB)[i] >= 1000000000000000000) {
+					(*AddB)[i] = (*AddB)[i] - 1000000000000000000;
 					Rest = 1;
 				}
 			}
-			for (unsigned long long i = (*AddA).size(); i < (*AddB).size(); i++) {
+			for (unsigned long long i  = (*AddA).size(); i < (*AddB).size(); i++) {
 				(*AddB)[i] = (*AddB)[i] + Rest;
 				Rest = 0;
-				if ((*AddB)[i] >= *CarryULL) {
-					(*AddB)[i] = (*AddB)[i] - *CarryULL;
+				if ((*AddB)[i] >= 1000000000000000000) {
+					(*AddB)[i] = (*AddB)[i] - 1000000000000000000;
 					Rest = 1;
 				}
 			}
 			if (Rest == 1) (*AddB).push_back(1);
 		}
 		else {
-			for (unsigned long long i = 0; i < (*AddB).size(); i++) {
+			for (unsigned long long i  = 0; i < (*AddB).size(); i++) {
 				(*AddB)[i] = (*AddA)[i] + (*AddB)[i] + Rest;
 				Rest = 0;
-				if ((*AddB)[i] >= *CarryULL) {
-					(*AddB)[i] = (*AddB)[i] - *CarryULL;
+				if ((*AddB)[i] >= 1000000000000000000) {
+					(*AddB)[i] = (*AddB)[i] - 1000000000000000000;
 					Rest = 1;
 				}
 			}
-			for (unsigned long long i = (*AddB).size(); i < (*AddA).size(); i++) {
+			for (unsigned long long i  = (*AddB).size(); i < (*AddA).size(); i++) {
 				(*AddB).push_back((*AddA)[i] + Rest);
 				Rest = 0;
-				if ((*AddB)[i] >= *CarryULL) {
-					(*AddB)[i] = (*AddB)[i] - *CarryULL;
+				if ((*AddB)[i] >= 1000000000000000000) {
+					(*AddB)[i] = (*AddB)[i] - 1000000000000000000;
 					Rest = 1;
 				}
 			}
@@ -211,49 +188,49 @@ namespace AddVectors {
 		(*Result).resize(0);
 
 		if ((*AddA).size() == (*AddB).size()) {
-			for (unsigned long long i = 0; i < (*AddA).size(); i++) {
+			for (unsigned long long i  = 0; i < (*AddA).size(); i++) {
 				(*Result).push_back((*AddA)[i] + (*AddB)[i] + Rest);
 				Rest = 0;
-				if ((*Result)[i] >= *CarryULL) {
-					(*Result)[i] = (*Result)[i] - *CarryULL;
+				if ((*Result)[i] >= 1000000000000000000) {
+					(*Result)[i] = (*Result)[i] - 1000000000000000000;
 					Rest = 1;
 				}
 			}
 			if (Rest == 1) (*Result).push_back(1);
 		}
 		else if ((*AddA).size() < (*AddB).size()) {
-			for (unsigned long long i = 0; i < (*AddA).size(); i++) {
+			for (unsigned long long i  = 0; i < (*AddA).size(); i++) {
 				(*Result).push_back((*AddA)[i] + (*AddB)[i] + Rest);
 				Rest = 0;
-				if ((*Result)[i] >= *CarryULL) {
-					(*Result)[i] = (*Result)[i] - *CarryULL;
+				if ((*Result)[i] >= 1000000000000000000) {
+					(*Result)[i] = (*Result)[i] - 1000000000000000000;
 					Rest = 1;
 				}
 			}
-			for (unsigned long long i = (*AddA).size(); i < (*AddB).size(); i++) {
+			for (unsigned long long i  = (*AddA).size(); i < (*AddB).size(); i++) {
 				(*Result).push_back((*AddB)[i] + Rest);
 				Rest = 0;
-				if ((*Result)[i] >= *CarryULL) {
-					(*Result)[i] = (*Result)[i] - *CarryULL;
+				if ((*Result)[i] >= 1000000000000000000) {
+					(*Result)[i] = (*Result)[i] - 1000000000000000000;
 					Rest = 1;
 				}
 			}
 			if (Rest == 1) (*Result).push_back(1);
 		}
 		else {
-			for (unsigned long long i = 0; i < (*AddB).size(); i++) {
+			for (unsigned long long i  = 0; i < (*AddB).size(); i++) {
 				(*Result).push_back((*AddA)[i] + (*AddB)[i] + Rest);
 				Rest = 0;
-				if ((*Result)[i] >= *CarryULL) {
-					(*Result)[i] = (*Result)[i] - *CarryULL;
+				if ((*Result)[i] >= 1000000000000000000) {
+					(*Result)[i] = (*Result)[i] - 1000000000000000000;
 					Rest = 1;
 				}
 			}
-			for (unsigned long long i = (*AddB).size(); i < (*AddA).size(); i++) {
+			for (unsigned long long i  = (*AddB).size(); i < (*AddA).size(); i++) {
 				(*Result).push_back((*AddA)[i] + Rest);
 				Rest = 0;
-				if ((*Result)[i] >= *CarryULL) {
-					(*Result)[i] = (*Result)[i] - *CarryULL;
+				if ((*Result)[i] >= 1000000000000000000) {
+					(*Result)[i] = (*Result)[i] - 1000000000000000000;
 					Rest = 1;
 				}
 			}
@@ -266,7 +243,7 @@ namespace AddVectors {
 	{
 		int Rest = 0;
 		unsigned long long UnsLL = pow(10, (*RestExponent));
-		for (unsigned long long i = 0; i < (*Adds).size(); i++) {
+		for (unsigned long long i  = 0; i < (*Adds).size(); i++) {
 			(*Adds)[i] = (*Adds)[i] + (*Adds)[i] + Rest;
 			Rest = 0;
 			if ((*Adds)[i] >= UnsLL) {
@@ -279,26 +256,44 @@ namespace AddVectors {
 
 	void AplusAonBwithCores3(std::vector<unsigned long long>* AddA, std::vector<unsigned long long>* AddB)
 	{
+		int Core1RestStartValue = 0;
+		int Core2RestStartValue = 0;
+		int Core3RestStartValue = 0;
+		int* Core3Rest = &Core3RestStartValue;
+		/*
+		int* Core1Rest = &Core1RestStartValue;
+		int* Core2Rest = &Core2RestStartValue;
+		*/
+		unsigned long long RangeStartValue0 = 0;
+		unsigned long long RangeStartValue1 = 0;
+		unsigned long long RangeStartValue2 = 0;
+		unsigned long long RangeStartValue3 = 0;
+		/*
+		unsigned long long* Range0 = &RangeStartValue0;
+		unsigned long long* Range1 = &RangeStartValue1;
+		unsigned long long* Range2 = &RangeStartValue2;
+		unsigned long long* Range3 = &RangeStartValue3;
+		*/
 		(*AddB).resize((*AddA).size());
 		if ((*AddA).size() < 3) {
 			AplusAonB(AddA, AddB);
 			return;
 		}
-		*Range0 = 0;
-		*Range1 = floor((*AddA).size() / 3) - 1;
-		*Range2 = 2 * (*Range1) + 1;
-		*Range3 = (*AddA).size() - 1;
+		RangeStartValue0 = 0;
+		RangeStartValue1 = floor((*AddA).size() / 3) - 1;
+		RangeStartValue2 = 2 * (RangeStartValue1) + 1;
+		RangeStartValue3 = (*AddA).size() - 1;
 
-		*Core1Rest = 0;
-		*Core2Rest = 0;
-		*Core3Rest = 0;
+		Core1RestStartValue = 0;
+		Core2RestStartValue = 0;
+		Core3RestStartValue = 0;
 
-		if ((*AddA)[*Range1] + (*AddA)[*Range1] >= *CarryULL) *Core2Rest = 1;
-		if ((*AddA)[*Range2] + (*AddA)[*Range2] >= *CarryULL) *Core3Rest = 1;
+		if ((*AddA)[RangeStartValue1] + (*AddA)[RangeStartValue1] >= 1000000000000000000) Core2RestStartValue = 1;
+		if ((*AddA)[RangeStartValue2] + (*AddA)[RangeStartValue2] >= 1000000000000000000) Core3RestStartValue = 1;
 
-		std::thread Core1(AplusAonBCore1, AddA, AddB, Range0, Range1, Core1Rest);
-		std::thread Core2(AplusAonBCore2, AddA, AddB, Range1, Range2, Core2Rest);
-		std::thread Core3(AplusAonBCore3, AddA, AddB, Range2, Range3, Core3Rest);
+		std::thread Core1(AplusAonBCore1, AddA, AddB, RangeStartValue0, RangeStartValue1, Core1RestStartValue);
+		std::thread Core2(AplusAonBCore2, AddA, AddB, RangeStartValue1, RangeStartValue2, Core2RestStartValue);
+		std::thread Core3(AplusAonBCore3, AddA, AddB, RangeStartValue2, RangeStartValue3, Core3Rest);
 
 		Core1.join();
 		Core2.join();
@@ -312,72 +307,80 @@ namespace AddVectors {
 	}
 	void AplusBonBwithCores3(std::vector<unsigned long long>* AddA, std::vector<unsigned long long>* AddB)
 	{
+		int Core1RestStartValue = 0;
+		int Core2RestStartValue = 0;
+		int Core3RestStartValue = 0;
+		int *Core3Rest = &Core3RestStartValue;
+		long long RangeStartValue0 = 0;
+		long long RangeStartValue1 = 0;
+		long long RangeStartValue2 = 0;
+		long long RangeStartValue3 = 0;
 		if ((*AddA).size() <= (*AddB).size()) {
 			if ((*AddA).size() < 3) {
 				AplusBonB(AddA, AddB);
 				return;
 			}
-			*Range0 = 0;
-			*Range1 = floor((*AddA).size() / 3) - 1;
-			*Range2 = 2 * (*Range1) + 1;
-			*Range3 = (*AddA).size() - 1;
+			RangeStartValue0 = 0;
+			RangeStartValue1 = floor((*AddA).size() / 3) - 1;
+			RangeStartValue2 = 2 * (RangeStartValue1) + 1;
+			RangeStartValue3 = (*AddA).size() - 1;
 		}
 		else {
 			if ((*AddB).size() < 3) {
 				AplusBonB(AddA, AddB);
 				return;
 			}
-			*Range0 = 0;
-			*Range1 = floor((*AddB).size() / 3) - 1;
-			*Range2 = 2 * (*Range1) + 1;
-			*Range3 = (*AddB).size() - 1;
+			RangeStartValue0 = 0;
+			RangeStartValue1 = floor((*AddB).size() / 3) - 1;
+			RangeStartValue2 = 2 * (RangeStartValue1) + 1;
+			RangeStartValue3 = (*AddB).size() - 1;
 		}
 
-		*Core1Rest = 0;
-		*Core2Rest = 0;
-		*Core3Rest = 0;
+		Core1RestStartValue = 0;
+		Core2RestStartValue = 0;
+		Core3RestStartValue = 0;
 
-		if ((*AddA)[*Range1] + (*AddB)[*Range1] >= *CarryULL) *Core2Rest = 1;
-		if ((*AddA)[*Range2] + (*AddB)[*Range2] >= *CarryULL) *Core3Rest = 1;
+		if ((*AddA)[RangeStartValue1] + (*AddB)[RangeStartValue1] >= 1000000000000000000) Core2RestStartValue = 1;
+		if ((*AddA)[RangeStartValue2] + (*AddB)[RangeStartValue2] >= 1000000000000000000) Core3RestStartValue = 1;
 
-		std::thread Core1(AplusBonBCore1, AddA, AddB, Range0, Range1, Core1Rest);
-		std::thread Core2(AplusBonBCore2, AddA, AddB, Range1, Range2, Core2Rest);
-		std::thread Core3(AplusBonBCore3, AddA, AddB, Range2, Range3, Core3Rest);
+		std::thread Core1(AplusBonBCore1, AddA, AddB, RangeStartValue0, RangeStartValue1, Core1RestStartValue);
+		std::thread Core2(AplusBonBCore2, AddA, AddB, RangeStartValue1, RangeStartValue2, Core2RestStartValue);
+		std::thread Core3(AplusBonBCore3, AddA, AddB, RangeStartValue2, RangeStartValue3, Core3Rest);
 
 		Core1.join();
 		Core2.join();
 		Core3.join();
 
-		if (*Core3Rest == 1) {
+		if (Core3RestStartValue == 1) {
 			if ((*AddA).size() == (*AddB).size()) {
 				(*AddB).push_back(1);
 			}
 			else if ((*AddA).size() > (*AddB).size()) {
-				for (unsigned long long i = *Range3 + 1; i < (*AddA).size(); i++) {
-					(*AddB).push_back((*AddA)[i] + (*Core3Rest));
-					if ((*AddB)[i] >= *CarryULL) {
-						(*AddB)[i] = (*AddB)[i] - *CarryULL;
+				for (long i = RangeStartValue3 + 1; i < (*AddA).size(); i++) {
+					(*AddB).push_back((*AddA)[i] + (Core3RestStartValue));
+					if ((*AddB)[i] >= 1000000000000000000) {
+						(*AddB)[i] = (*AddB)[i] - 1000000000000000000;
 						//*Core3Rest = 1;
 					}
 					else {
-						*Core3Rest = 0;
+						Core3RestStartValue = 0;
 						break;
 					}
 				}
-				if (*Core3Rest == 1) (*AddB).push_back(1);
+				if (Core3RestStartValue == 1) (*AddB).push_back(1);
 			}
 			else {
-				for (unsigned long long i = *Range3 + 1; i < (*AddB).size(); i++) {
-					(*AddB)[i] = (*AddB)[i] + (*Core3Rest);
-					if ((*AddB)[i] >= *CarryULL) {
-						(*AddB)[i] = (*AddB)[i] - *CarryULL;
+				for (unsigned long long i  = RangeStartValue3 + 1; i < (*AddB).size(); i++) {
+					(*AddB)[i] = (*AddB)[i] + (Core3RestStartValue);
+					if ((*AddB)[i] >= 1000000000000000000) {
+						(*AddB)[i] = (*AddB)[i] - 1000000000000000000;
 					}
 					else {
-						*Core3Rest = 0;
+						Core3RestStartValue = 0;
 						break;
 					}
 				}
-				if (*Core3Rest == 1) (*AddB).push_back(1);
+				if (Core3RestStartValue == 1) (*AddB).push_back(1);
 			}
 		}
 	}
