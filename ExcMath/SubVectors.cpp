4db182743d46df4cdc463620fed9968398d798d4
Kutrowski <Dominik.Kutrowski@dnvgl.com>
2020-12-05 23:28:24 +0100

    SubVectors:  - SubVectors::BminusAonB(SubB, SubA),  - no tests/not tested,

--- ExcMath/SubVectors.cpp
+++ ExcMath/SubVectors.cpp
@@ -2,5 +2,50 @@
 
 void SubVectors::BminusAonB(std::vector<unsigned long long>* SubB, std::vector<unsigned long long>* SubA)
 {
-	//soon...
+	int Rest = 0;
+	if ((*SubA).size() == (*SubB).size()) {
+		for (unsigned long long i = 0; i < (*SubA).size(); i++) {
+			if ((*SubB)[i] >= (*SubA)[i]) {
+				(*SubB)[i] = (*SubB)[i] - (*SubA)[i] - Rest;
+				Rest = 0;
+			}
+			else {
+				(*SubB)[i] = (*SubB)[i] - (*SubA)[i] - Rest + 1000000000000000000;
+				Rest = 1;
+			}
+		}
+		for (unsigned long long i = (*SubB).size() - 1; i >= 1; i--) {
+			if ((*SubB)[i] == 0) (*SubB).pop_back();
+			else break;
+		}
+	}
+	else if ((*SubB).size() > (*SubA).size()) {
+		for (unsigned long long i = 0; i < (*SubA).size(); i++) {
+			if ((*SubB)[i] >= (*SubA)[i]) {
+				(*SubB)[i] = (*SubB)[i] - (*SubA)[i] - Rest;
+				Rest = 0;
+			}
+			else {
+				(*SubB)[i] = (*SubB)[i] - (*SubA)[i] - Rest + 1000000000000000000;
+				Rest = 1;
+			}
+		}
+		if (Rest == 1) {
+			for (unsigned long long i = (*SubA).size(); i <= (*SubB).size() - 1; i++) {
+				(*SubB)[i] = (*SubB)[i] - Rest;
+				if ((*SubB)[i] >= 1000000000000000000) {
+					(*SubB)[i] = (*SubB)[i] - 1000000000000000000;
+					Rest = 1;
+				}
+				else break;
+			}
+			for (unsigned long long i = (*SubB).size() - 1; i >= 1; i--) {
+				if ((*SubB)[i] == 0) (*SubB).pop_back();
+				else break;
+			}
+		}
+	}
+	else {
+		//add not possible?
+	}
 }
