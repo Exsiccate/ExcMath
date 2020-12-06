a55eea2a46d6390bdb8115bfcdd472f76c4a9f6e
Kutrowski <Dominik.Kutrowski@dnvgl.com>
2020-12-06 23:01:58 +0100

    SubVectors::BsubAonB(SubB, SubA): - unit test added, - new tests specified to add,

--- UnitTestAddVector/UnitTestsSubVectors.cpp
+++ UnitTestAddVector/UnitTestsSubVectors.cpp
@@ -12,12 +12,30 @@ namespace UnitTestSub {
 		public:
 			TEST_METHOD(Should_Sub_Correctly) {
 				//given
-				// ...
+				std::vector<unsigned long long> VecResult;
+				std::vector<unsigned long long>* VecToCheckResult = &VecResult;
+				std::vector<unsigned long long> VectorSubB;
+				std::vector<unsigned long long>* VecSubB = &VectorSubB;
+				std::vector<unsigned long long> VectorSubA;
+				std::vector<unsigned long long>* VecSubA = &VectorSubA;
+				(*VecSubB).push_back(100);
+				(*VecSubB).push_back(100);
+				(*VecSubA).push_back(50);
+				(*VecSubA).push_back(50);
+				(*VecToCheckResult).push_back(50);
+				(*VecToCheckResult).push_back(50);
 				//when
-				// ...
+				SubVectors::BsubAonB(VecSubB, VecSubA);
 				//then
-				// ...
+				Assert::AreEqual((*VecToCheckResult)[0], (*VecSubB)[0]);
+				Assert::AreEqual((*VecToCheckResult)[1], (*VecSubB)[1]);
 			}
+			TEST_METHOD(Should_Sub_Zeros){}
+			TEST_METHOD(Should_Sub_Correctly_When_VectorB_Is_Equal_VectorA){}
+			TEST_METHOD(Should_Sub_Correctly_When_VectorB_Is_Longer_Than_VectorA) {}
+			TEST_METHOD(Should_Sub_Correctly_When_VectorB_And_VectorS_Is_Equal_Max_Value) {}
+			TEST_METHOD(Should_Sub_One_Empty) {}
+			TEST_METHOD(Should_Sub_Both_Empty) {}
 		};
 	}
 }
